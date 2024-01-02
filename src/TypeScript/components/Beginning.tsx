76f873524609e5f35ec55db568ebf9e39231e785
rsun19 <robertssun1234@gmail.com>
2024-01-02 17:08:29 -0500

    styled button

--- src/TypeScript/components/Beginning.tsx
+++ src/TypeScript/components/Beginning.tsx
@@ -53,8 +53,8 @@ const Beginning = () => {
         </div>
         {!finishAnimation && <div style={{ margin: '0px', padding: '0px'}}>
           <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
-            <button id='portfolioButton' style={{ position: 'absolute', backgroundColor: 'lightgray', cursor: 'pointer', zIndex: '1'}} onClick={startAnimation}>
-                <p style={{ paddingLeft: '1rem', paddingRight: '1rem' }}>View Portfolio Page!</p>
+            <button id='portfolioButton' className="bg-blue-500 hover:bg-blue-700 text-white 2xl:text-2xl 4xl:text-3xl 5xl:text-4xl font-bold py-2 px-4 rounded-full" style={{ position: 'absolute', zIndex: '1'}} onClick={startAnimation}>
+                View Portfolio Page!
             </button>
           <div style={{ height: '100vh', width: '100vw', margin: '0px', padding: '0px'}}>
             <Canvas>
