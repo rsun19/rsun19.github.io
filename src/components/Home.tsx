244fb6d22528ba3fb25a8e6156dbef0e40e39182
rsun19 <robertssun1234@gmail.com>
2024-04-25 20:15:38 -0400

    fixed import

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import Typing from "./Typing";
 import useWidth from "../hooks/changeWidth";
 import PortfolioSlider from "./PortfolioSlider";
