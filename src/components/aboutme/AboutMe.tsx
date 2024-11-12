235f36e2a16f778b1b6cd6fd5fedff1c4ce51422
rsun19 <robertssun1234@gmail.com>
2024-11-11 23:44:51 -0500

    fixed typo

--- src/components/aboutme/AboutMe.tsx
+++ src/components/aboutme/AboutMe.tsx
@@ -9,7 +9,7 @@ export default function AboutMe() {
         </h1>
         <Text size="md">
             Hi! I'm a fourth-year Computer Science student at the University of Virginia! Through my work and educational
-            experiences, I've gained exposure to a variety of fields from full stack development to artifical intelligence
+            experiences, I've gained exposure to a variety of fields from full stack development to artificial intelligence
             and statistics. Plus, I'm taking a class right now called "Alien Worlds." 10/10 highly recommend.
             <br /><br />
             When I'm not in front of a code editor, I like learning Chinese and German on Duolingo, watching tv shows on Netflix, 
